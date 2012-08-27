0541ebc60903d63f86c017ed204720b9644182c8
thuntn <thuntn@exoplatform.com>
2012-08-27 17:55:10 +0700

    2 case create podcast document

--- ECMS_DMS_SE_createPodcast.java
+++ ECMS_DMS_SE_createPodcast.java
@@ -52,8 +52,81 @@ public class ECMS_DMS_SE_createPodcast extends EcmsBase {
     	pause(2000);
     	
     }
+}
+@Test(groups={"general","content"})
+public void addPodcastDocumentinContentFolder() {
 	
+	//create a content folder
+	try {
+		createNewContentFolder("myfolder", "");
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	action.doubleClick(waitForAndGetElementByLinkText("myfolder")).build().perform();
+  //create a new podcast
+	gotoAddnewConntent();
+  createNewPodcast("podcast", "podcast", "hay365.net");
+  //check result for success
+
+  waitForAndGetElementByXpath("//div[@title='Sites Management']").click();
+  pause(2000);
+  if(isElementPresent(By.linkText("myfolder")))
+  {
+  	System.out.println("Create a content folder successfully!");
+  	pause(1000);
+  	action.doubleClick(waitForAndGetElementByLinkText("myfolder")).build().perform();
+  	pause(2000);
+  	if(isElementPresent(By.linkText("podcast")))
+  	{	System.out.println("Create a podcast document successfully!");
+  	//delete podcast
+  	deleteNode("podcast ");
+  	}
+  	pause(4000);
+  	//delete article document
+  	waitForAndGetElementByXpath("//div[@title='Sites Management']").click();
+  	pause(3000);
+  	deleteNode("myfolder ");
+  	pause(2000);
+  	
   }
+	
+}
+//add a podcast document in a document folder
+@Test(groups={"general","document"})
+public void addPodcastDocumentinDocumentFolder() {
+	
+	//create a document folder
+    createNewDocumentFolder("mydocumentfolder","");
+	action.doubleClick(waitForAndGetElementByLinkText("mydocumentfolder")).build().perform();
+  //create a new podcast
+	gotoAddnewConntent();
+  createNewPodcast("podcast", "podcast", "hay365.net");
+  //check result for success
+
+  waitForAndGetElementByXpath("//div[@title='Sites Management']").click();
+  pause(2000);
+  if(isElementPresent(By.linkText("mydocumentfolder")))
+  {
+  	System.out.println("Create a content folder successfully!");
+  	pause(1000);
+  	action.doubleClick(waitForAndGetElementByLinkText("mydocumentfolder")).build().perform();
+  	pause(2000);
+  	if(isElementPresent(By.linkText("podcast")))
+  	{	System.out.println("Create a podcast document successfully!");
+  	//delete podcast
+  	deleteNode("podcast ");
+  	}
+  	pause(4000);
+  	//delete article document
+  	waitForAndGetElementByXpath("//div[@title='Sites Management']").click();
+  	pause(3000);
+  	deleteNode("mydocumentfolder ");
+  	pause(2000);
+  	
+  }
+	
+}
   @BeforeSuite(groups={"general","article"})
   public void beforeSuite() {
 	  driver.manage().window().maximize();
